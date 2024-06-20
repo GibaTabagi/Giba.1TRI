a655817a7bc2218a9d71ed8a53601474868ec7ca
GibaTabagi <167425780+GibaTabagi@users.noreply.github.com>
2024-06-20 08:00:17 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
 const tempo= document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date(20)
+const tempoObjetivo2 = new Date(10)
+const tempoObjetivo3 = new Date(05)
+const tempoObjetivo4 = new Date(60)
+const agora= new date ();
+let segundo;
+segundo=(tempoObjetivo1-agora)/1000
+let minuto;
+minuto=segundo/60
+let horas;
+horas=minutos/60
+let dias;
+dias=horas/24
+tempo[0].textContent = tempoObjetivo1-agora
 for(let i = 0; 1 < botoes.length ; i++){
     botoes[i].onclick = function() {
         for (j=0;j<botoes.length;j++){

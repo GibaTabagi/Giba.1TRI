68a39eaf4e9c1c813364097ec70cb6d978b355a9
GibaTabagi <167425780+GibaTabagi@users.noreply.github.com>
2024-07-04 08:10:22 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,54 @@
 const botoes = document.querySelectorAll(".botao");
-const conteudo = document.querySelectorAll(".aba-conteudo");
-const tempo= document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date(20)
-const tempoObjetivo2 = new Date(10)
-const tempoObjetivo3 = new Date(05)
-const tempoObjetivo4 = new Date(60)
-const agora= new date ();
-let segundo;
-segundo=(tempoObjetivo1-agora)/1000
-let minuto;
-minuto=segundo/60
-let horas;
-horas=minutos/60
+const conteudo = document.querySelectorAll(".abas-conteudo");
+const tempo = document.querySelectorAll(.contador);
+const tempoObjetivo1 new Date(2024, 7, 29);
+const tempoObjetivo2 new Date(2024, 8, 29);
+const tempoObjetivo3 new Date(2024, 9, 29);
+const tempoObjetivo4 new Date(2024, 10, 29);
+
+let segundos;
+let minutos;
+let horas; 
 let dias;
-dias=horas/24
-tempo[0].textContent = tempoObjetivo1-agora
-for(let i = 0; 1 < botoes.length ; i++){
+
+let segundos2;
+let minutos2;
+let horas2; 
+let dias2;
+
+let segundos3;
+let minutos3;
+let horas3; 
+let dias3;
+
+let segundos4;
+let minutos4;
+let horas4; 
+let dias4;
+
+const agora = new Date();
+tempo[0].textContent = tempoObjetivo1 - agora;
+segundos = (tempoObjetivo - agora)/1000;
+minutos = segundos/60;
+horas = minutos /60;
+dias = horas/24;
+
+segundos = Math.floor(segundos);
+minutos = Math.floor(minutos);
+horas = Math.floor(horas);
+dias = Math.floor(dias);
+
+tempo[0].textContent = 'faltam ${dias}', dias, ${horas}, horas, ${minutos}, minutos e ${segundos} segundos'
+
+console.log(tempo);
+console.log(botoes);
+for (let i = 0; i <  botoes.length; i++) {
     botoes[i].onclick = function() {
-        for (j=0;j<botoes.length;j++){
+        for (let j = 0;j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             conteudo[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo")
-        conteudo[i].classList.add("ativo")
+       conteudo[i].classList.add("ativo")
     }
 }
